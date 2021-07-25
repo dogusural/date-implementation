70486605acaef4aa4e3c851e0557f1b33d73620e
dogusural <dogusural@gmail.com>
2021-07-25 14:12:23 +0200

    added actual test cases

--- source/test/test.cpp
+++ source/test/test.cpp
@@ -1,23 +1,43 @@
 #include "Date.h"
 
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp" // Header only library from 
+#include "catch.hpp"      // Header only library from
 // https://web.archive.org/web/20170807171031/https://raw.githubusercontent.com/philsquared/Catch/master/single_include/catch.hpp
 
 #include <iostream>
 
-
-project::Date my_date{1, 1, 2020};
-project::Date my_copy_date = my_date;
-unsigned int Factorial(unsigned int number)
+project::Date pre_increment(project::Date date)
 {
-    return number <= 1 ? number : Factorial(number - 1) * number;
+    return ++date;
 }
 
-TEST_CASE("Factorials are computed", "[factorial]")
+project::Date post_increment(project::Date date)
 {
-    REQUIRE(Factorial(1) == 1);
-    REQUIRE(Factorial(2) == 2);
-    REQUIRE(Factorial(3) == 6);
-    REQUIRE(Factorial(10) == 3628800);
+    return date++;
+}
+
+project::Date addition(project::Date date, int days)
+{
+    return date + days;
+}
+
+TEST_CASE("Pre increment test", "[pre_increment]")
+{
+    project::Date my_date{1, 1, 2020};
+
+    REQUIRE(pre_increment(my_date) > my_date);
+}
+
+TEST_CASE("Post increment test", "[post_increment]")
+{
+    project::Date my_date{1, 1, 2020};
+
+    REQUIRE(post_increment(my_date) == my_date);
+}
+
+TEST_CASE("Addition test", "[addition]")
+{
+    project::Date my_date{28, 2, 2020};
+
+    REQUIRE(addition(my_date , 10) == (project::Date{9,3,2020}) );
 }
