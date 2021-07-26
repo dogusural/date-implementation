07ddc8a19ea73a8b037e289ae1313e0ba3d88c3b
dogusural <dogusural@gmail.com>
2021-07-26 18:03:16 +0200

    added a new test for logic operators

--- source/test/test.cpp
+++ source/test/test.cpp
@@ -24,11 +24,22 @@ int substraction(project::Date first_date, project::Date second_date)
 {
     return first_date - second_date;
 }
+bool logic_operators()
+{
+    bool result = false;
+    project::Date date = project::Date::random_date();
+    std::cout << "Randomly generated date for the logic_operatos test :" << date << std::endl;
+    result &= (date + 1 == 1 + date);
+    result &= (date + 1 > date);
+    result &= (date -3 >= date -2);
+    result &= (date -100 < date - 99);
+    return result;
+}
 
 TEST_CASE("substraction test", "[substraction]")
 {
-    REQUIRE(substraction((project::Date{31, 12, 2020}),(project::Date{1, 1, 2020})) == 365);
-    REQUIRE(substraction((project::Date{31, 12, 2021}),(project::Date{1, 1, 2021})) == 364);
+    REQUIRE(substraction((project::Date{31, 12, 2020}), (project::Date{1, 1, 2020})) == 365);
+    REQUIRE(substraction((project::Date{31, 12, 2021}), (project::Date{1, 1, 2021})) == 364);
 }
 TEST_CASE("Pre increment test", "[pre_increment]")
 {
@@ -43,6 +54,11 @@ TEST_CASE("Post increment test", "[post_increment]")
 TEST_CASE("Addition test", "[addition]")
 {
     REQUIRE(addition((project::Date{28, 2, 2020}), 10) == (project::Date{9, 3, 2020}));
-    REQUIRE(addition((project::Date{28, 2, 2021}), 10) == (project::Date{10, 3, 2020}));
+    REQUIRE(addition((project::Date{28, 2, 2021}), 10) == (project::Date{10, 3, 2021}));
     REQUIRE(addition((project::Date{28, 12, 2005}), 10) == (project::Date{7, 1, 2006}));
+}
+
+TEST_CASE("Logic operators test", "[logic_operators]")
+{
+    REQUIRE(logic_operators() == (true));
 }
